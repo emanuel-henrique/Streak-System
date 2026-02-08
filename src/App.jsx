adc3f95d8ccfce14e1e25d88506f1a62fe10fbb7
Emanuel Henrique <emanuelhenrique1562@gmail.com>
2026-02-08 19:58:32 -0300

    Add streak count

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,29 @@ import { useEffect, useState } from "react";
 import { Calendar } from "@/components/ui/calendar";
 import { ptBR } from "date-fns/locale";
 import { Button } from "@/components/ui/button";
+import { differenceInDays } from "date-fns";
 
 export default function App() {
   const [date, setDate] = useState();
+  const [streak, setStreak] = useState(0);
 
   function handleClick() {
     const today = new Date();
 
     if (!localStorage.getItem("@streakCalendar:from")) {
+      setDate({ from: today, to: null });
       localStorage.setItem("@streakCalendar:from", today.getTime());
-      return setDate({ from: today, to: null });
+      localStorage.setItem("@streakCalendar:streakDays", 1);
+      return setStreak(1);
     }
 
-    setDate((prev) => ({ ...prev, to: today }));
+    const fromDate = new Date(+localStorage.getItem("@streakCalendar:from"));
+    const streakDays = differenceInDays(today, fromDate) + 1;
 
+    setDate((prev) => ({ ...prev, to: today }));
+    setStreak(streakDays);
     localStorage.setItem("@streakCalendar:to", today.getTime());
+    localStorage.setItem("@streakCalendar:streakDays", streakDays);
   }
 
   useEffect(() => {
@@ -31,13 +39,18 @@ export default function App() {
         from: fromDate,
         to: toDate,
       });
+
+      const streakDays = toDate ? differenceInDays(toDate, fromDate) + 1 : 1;
+      setStreak(streakDays);
     }
   }, []);
 
   return (
     <div className="h-dvh flex flex-col items-center pt-20">
       <h1 className="text-3xl mb-5">Streak System</h1>
-
+      <p className="text-xl mb-3 font-semibold">
+        🔥 {streak} {streak === 1 ? "dia" : "dias"}
+      </p>
       <Calendar
         mode="range"
         locale={ptBR}
